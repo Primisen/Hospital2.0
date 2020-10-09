9d78dada274fae9a2098722fd8b1948f464226c8
Nadia <primisen17@gmail.com>
2020-10-09 12:49:48 +0300

    Delete MedicalStaffDao type

--- src/main/java/training/nadia/hospital/dao/MedicalStaffDao.java
+++
@@ -1,8 +0,0 @@
-package training.nadia.hospital.dao;
-
-import training.nadia.hospital.dao.exception.DaoException;
-
-public interface MedicalStaffDao {
-
-    int getStaffTypeId(String login) throws DaoException;
-}
