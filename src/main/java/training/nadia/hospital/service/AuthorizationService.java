b2c13eecee18415f6b0977d056e90d5d28812d6e
Nadia <primisen17@gmail.com>
2020-10-19 12:31:56 +0300

    Correct authorization logic

--- src/main/java/training/nadia/hospital/service/AuthorizationService.java
+++ src/main/java/training/nadia/hospital/service/AuthorizationService.java
@@ -1,10 +1,10 @@
 package training.nadia.hospital.service;
 
-import training.nadia.hospital.entity.MedicalStaff;
 import training.nadia.hospital.entity.User;
 import training.nadia.hospital.service.exception.ServiceException;
 
 public interface AuthorizationService {
 
     User authorize(String login, String password) throws ServiceException;
+
 }
--- src/main/java/training/nadia/hospital/service/impl/AuthorizationServiceImpl.java
+++ src/main/java/training/nadia/hospital/service/impl/AuthorizationServiceImpl.java
@@ -1,5 +1,7 @@
 package training.nadia.hospital.service.impl;
 
+import training.nadia.hospital.dao.AuthorizationDao;
+import training.nadia.hospital.dao.UtilDao;
 import training.nadia.hospital.dao.exception.DaoException;
 import training.nadia.hospital.dao.impl.*;
 import training.nadia.hospital.entity.*;
@@ -8,52 +10,35 @@ import training.nadia.hospital.service.exception.ServiceException;
 
 public class AuthorizationServiceImpl implements AuthorizationService {
 
-    private AuthorizationDaoImpl authorizationDao = new AuthorizationDaoImpl();
-    private UserDaoImpl userDao = new UserDaoImpl();
+    private AuthorizationDao authorizationDao = new AuthorizationDaoImpl();
 
     @Override
     public User authorize(String login, String password) throws ServiceException {
 
+
         try {
 
-            User user = authorizationDao.getData(login);
+            if (isUserExist(login, password)) {
 
-            if (!user.getPassword().equals(password)) {
-                throw new ServiceException("Неправильно введен логин или пароль!");//!
+                User user = authorizationDao.getUserData(login);
+                return user;
 
-            } else if (user.getRole().equals(Role.PATIENT)) {
-                Patient patient = new Patient();
-                copy(user, patient);
-                authorizationDao.getData(patient);
-                return patient;
-
-            } else if (user.getRole().equals(Role.DOCTOR)) {
-                Doctor doctor = new Doctor();
-                copy(user, doctor);
-                authorizationDao.getData(doctor);
-                return doctor;
-
-            } else if (user.getRole().equals(Role.NURSE)) {
-                Nurse nurse = new Nurse();
-                copy(user, nurse);
-                authorizationDao.getData(nurse);
-                return nurse;
+            } else {
+                throw new ServiceException("Неправильно введен логин или пароль");
             }
 
         } catch (DaoException e) {
             throw new ServiceException(e.getMessage());
         }
 
-        return null;
     }
 
-    private void copy(User source, User destination) {
+    private boolean isUserExist(String login, String password) throws DaoException {
 
-        destination.setRole(source.getRole());
-        destination.setId(source.getId());
-        destination.setName(source.getName());
-        destination.setSurname(source.getSurname());
-        destination.setLogin(source.getLogin());
-        destination.setPassword(source.getPassword());
+        UtilDao utilDao = new UtilDaoImpl();
+
+        boolean userNotExist = utilDao.getUser(login, password) == null;
+
+        return !userNotExist;
     }
 }
