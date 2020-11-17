303515014d09e8ec99a875e94edb9febb428f773
Nadia <primisen17@gmail.com>
2020-11-17 16:44:51 +0300

    Make BasicConnectionPool is singletop and clean up code in this class

--- src/main/java/training/nadia/hospital/util/db/BasicConnectionPool.java
+++ src/main/java/training/nadia/hospital/util/db/BasicConnectionPool.java
@@ -6,34 +6,42 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BasicConnectionPool implements ConnectionPool { //синглтон, сделать статическим
+public class BasicConnectionPool implements ConnectionPool {
+
+    private static BasicConnectionPool instance;
 
     private String url;
     private String user;
     private String password;
-    private static List<Connection> connectionPool;
+    private List<Connection> connectionPool;
     private List<Connection> usedConnections = new ArrayList<>();
-    private static int INITIAL_POLL_SIZE = 10;
+    private static final int INITIAL_POLL_SIZE = 10;
 
-    public BasicConnectionPool(String url, String user, String password, List<Connection> pool) {
+    private BasicConnectionPool(String url, String user, String password, List<Connection> connectionPool) {
         this.url = url;
         this.user = user;
         this.password = password;
-//        connectionPool = new ArrayList<>();
+        this.connectionPool = connectionPool;
     }
 
     public static BasicConnectionPool create(String url, String user, String password) throws SQLException {
 
-        connectionPool = new ArrayList<>(INITIAL_POLL_SIZE);
-        for (int i = 0; i < INITIAL_POLL_SIZE; i++) {
-            connectionPool.add(createConnection(url, user, password));
+        if (instance == null) {
+
+            List<Connection> pool = new ArrayList<>(INITIAL_POLL_SIZE);
+            for (int i = 0; i < INITIAL_POLL_SIZE; i++) {
+                pool.add(createConnection(url, user, password));
+            }
+
+            instance = new BasicConnectionPool(url, user, password, pool);
         }
 
-        return new BasicConnectionPool(url, user, password, connectionPool);
+        return instance;
     }
 
     @Override
-    public Connection getConnection() {//проверка на колво, а то выпаду в -1
+    public Connection getConnection() {
+
         Connection connection = connectionPool.remove(connectionPool.size() - 1);
         usedConnections.add(connection);
         return connection;
@@ -43,11 +51,6 @@ public class BasicConnectionPool implements ConnectionPool { //синглтон,
     public boolean releaseConnection(Connection connection) {
         connectionPool.add(connection);
         return usedConnections.remove(connection);
-
-    }
-
-    private static Connection createConnection(String url, String user, String password) throws SQLException {
-        return DriverManager.getConnection(url, user, password);
     }
 
     public int getSize() {
@@ -59,6 +62,7 @@ public class BasicConnectionPool implements ConnectionPool { //синглтон,
         return url;
     }
 
+    @Override
     public String getUser() {
         return user;
     }
@@ -68,4 +72,7 @@ public class BasicConnectionPool implements ConnectionPool { //синглтон,
         return password;
     }
 
+    private static Connection createConnection(String url, String user, String password) throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
 }
